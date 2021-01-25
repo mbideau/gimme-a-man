9a2ff256c7d1a0b697b260ff1be521403cb46fb0
Michael Bideau <mica.devel@gmail.com>
2021-01-25 18:16:02 +0100

    MarkdownLint: fixed typo in conf file

--- .mdl.rb
+++ .mdl.rb
@@ -2,7 +2,7 @@
 all
 
 # Extend line length
-rule 'MD013', :line_length => 100, :code_blocks => False
+rule 'MD013', :line_length => 100, :code_blocks => false
 
 # Allow two trailing space (for forcing line break)
 rule 'MD009', :br_spaces => 2
